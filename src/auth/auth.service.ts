073b20b9397ad48eabaae457f3dc213332607f26
Mahmoud Ayesh <mahayesh7@gmail.com>
2026-04-19 20:39:14 +0300

    feat(auth): implement register method with password hashing

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,17 @@
+import { User } from '@/database/entities/user.entity';
+import { CreateUserDto } from '@/users/dto/create-users.dto';
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
+import { UsersService } from '@/users/users.service';
 
 @Injectable()
 export class AuthService {
   // Implement authentication logic here
-  constructor(private jwtService: JwtService) {}
+  constructor(
+    private jwtService: JwtService,
+    private usersService: UsersService,
+  ) {}
 
   private async hashPassword(password: string): Promise<string> {
     const hashPassword = await bcrypt.hash(password, 10);
@@ -15,5 +21,14 @@ export class AuthService {
   private async validatePassword(password: string, hashPassword: string): Promise<boolean> {
     const isValid = await bcrypt.compare(password, hashPassword);
     return isValid;
+  }
+
+  async register(createUserDto: CreateUserDto): Promise<User> {
+    const hashedPassword = await this.hashPassword(createUserDto.password);
+    const user = await this.usersService.createUser({
+      ...createUserDto,
+      password: hashedPassword,
+    });
+    return user;
   }
 }
