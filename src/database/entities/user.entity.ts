066c3f5020d05e9f0c40ea39b07ccfc9493d4ccd
Mahmoud Ayesh <mahayesh7@gmail.com>
2026-04-19 12:42:46 +0300

    feat(auth): create User entity with JWT fields

---
+++ src/database/entities/user.entity.ts
@@ -0,0 +1,34 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  fullName: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ nullable: false })
+  password: string;
+
+  @Column({ type: 'enum', enum: ['user', 'admin'], default: 'user' })
+  role: 'user' | 'admin';
+
+  @Column({ nullable: true })
+  refresh_token_hash: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
