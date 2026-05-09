107843e9974b2def4644f1e52baca4b6f974e0b2
Mahmoud Ayesh <mahayesh7@gmail.com>
2026-05-09 14:47:53 +0300

    fix:RequestIdInterceptor remeoved tap since this is a response

--- src/common/interceptors/request-id.interceptor.ts
+++ src/common/interceptors/request-id.interceptor.ts
@@ -1,7 +1,6 @@
 import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import * as crypto from 'crypto';
-import { tap } from 'rxjs/operators';
 import { Response } from 'express';
 
 @Injectable()
@@ -9,10 +8,7 @@ export class RequestIdInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<unknown> {
     const response = context.switchToHttp().getResponse<Response>();
     const requestId = crypto.randomUUID();
-    return next.handle().pipe(
-      tap(() => {
-        response.header('X-Request-ID', requestId);
-      }),
-    );
+    response.header('X-Request-ID', requestId);
+    return next.handle();
   }
 }
